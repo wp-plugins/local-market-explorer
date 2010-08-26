a5e8749974dbed52f95a8056af9bb125236d963d
amattie <amattie@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2010-08-26 06:56:06 +0000

    added branding link and donate button
    git-svn-id: https://plugins.svn.wordpress.org/local-market-explorer/branches/3.0@280858 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- admin.php
+++ admin.php
@@ -305,7 +305,8 @@ HTML;
 									<li>nileguide</li>
 								</ul>
 								<p>
-									Note that dsIDXpress is NOT included in that list as it has its own shortcodes you can use.
+									Note that local real estate via <a href="http://www.dsidxpress.com">dsIDXpress</a> is
+									NOT included in that list as it has its own shortcodes you can use.
 								<p>
 									For the location attributes, you'll need to specify either "city" and "state",
 									"neighborhood" "city" and "state", or "zip". You'll want to use the full names
@@ -342,6 +343,13 @@ HTML;
 							<a href="http://localmarketexplorer.uservoice.com/">User Voice forum</a></p>
 							
 							<p>- Andrew Mattie, plugin author</p>
+							
+							<form action="https://www.paypal.com/cgi-bin/webscr" method="post">
+							<input type="hidden" name="cmd" value="_s-xclick">
+							<input type="hidden" name="hosted_button_id" value="AUU3XFCCEKX4S">
+							<input type="image" src="https://www.paypal.com/en_US/i/btn/btn_donateCC_LG.gif" border="0" name="submit" alt="PayPal - The safer, easier way to pay online!">
+							<img alt="" border="0" src="https://www.paypal.com/en_US/i/scr/pixel.gif" width="1" height="1">
+							</form>
 						</div>
 					</div>
 				</div>
