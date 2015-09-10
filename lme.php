a7bdb23cf08496063620eecf6d50d6f9a1e2d27d
dustin999 <dustin999@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2015-09-10 19:36:45 +0000

    Testing Dustin credentials
    git-svn-id: https://plugins.svn.wordpress.org/local-market-explorer/trunk@1242744 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- lme.php
+++ lme.php
@@ -24,7 +24,8 @@ Author: Andrew Mattie & Jonathan Mabe
     Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
 */
 
-global $wp_version, $wpdb;
+//Test
+global $wp_version, $wpdb;     
 
 require_once(ABSPATH . "wp-admin/includes/plugin.php");
 $pluginData = get_plugin_data(__FILE__);
